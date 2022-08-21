e6b1194e106c413280c9ac511687d7e5700a6bfb
connork96 <connorkelly96@yahoo.com>
2022-08-20 21:43:30 -0400

    created start game function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var time = 75;
-var startButton = document.getElementById("start-btn");
+var startButton = document.getElementById("startBtn");
 var questionContainerEl = document.getElementById("questions-container");
 var startContainerEl = document.getElementById("starting-container");
 var questionEl = document.getElementById("question");
 var answerButtonsEl = document.getElementById("choice-buttons");
+
+var shuffledQuestions, currentQuestions
+
