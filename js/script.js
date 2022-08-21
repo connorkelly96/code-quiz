09df71875580c94d3eb2d6ab95c1b8cf7873a5b0
connork96 <connorkelly96@yahoo.com>
2022-08-20 21:25:25 -0400

    created starting variables in js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var time = 75;
+var startButton = document.getElementById("start-btn");
+var questionContainerEl = document.getElementById("questions-container");
+var startContainerEl = document.getElementById("starting-container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("choice-buttons");
